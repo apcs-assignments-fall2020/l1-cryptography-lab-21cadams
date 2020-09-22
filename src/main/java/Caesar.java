1f242097f4fe4088c52e176dbaa97dd6293a2af1
21cadams <21cadams@athenian.org>
2020-09-22 18:46:00 +0000

    Caesar Cipher part 1

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -2,23 +2,61 @@ import java.util.Scanner;
 
 public class Caesar {
     public static String encryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
-    }
+        String end ="";
+        for( int i = 0 ; i< message.length(); i++){
+            if (message.charAt(i)> 64 && message.charAt(i)< 91){
+                int a = ((message.charAt(i)-62)%26) +65;
+                end = end + (char)(a);
+            }else if (message.charAt(i)> 96 && message.charAt(i)< 123){
+                int a = ((message.charAt(i)-94)%26) +97;
+                end = end + (char)(a);
+            } else{
+                end = end + message.charAt(i);
+            }  
+        }
+        return end;
+    }   
 
     public static String decryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String end ="";
+        for( int i = 0 ; i< message.length(); i++){
+            if (message.charAt(i)> 64 && message.charAt(i)< 91){
+                int a = ((message.charAt(i)-68)%26) +65;
+                end = end + (char)(a);
+            }else{
+                end = end + message.charAt(i);
+            }   
+        }
+        return end;
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String end ="";
+        for( int i = 0 ; i< message.length(); i++){
+            if (message.charAt(i)> 64 && message.charAt(i)< 91){
+                int a = ((message.charAt(i)+ key-65)%26) +65;
+                end = end + (char)(a);
+            }else if (message.charAt(i)> 96 && message.charAt(i)< 123){
+                int a = ((message.charAt(i)+key-97)%26) +97;
+                end = end + (char)(a);
+            } else{
+                end = end + message.charAt(i);
+            }  
+        }
+        return end;
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String end ="";
+        for( int i = 0 ; i< message.length(); i++){
+            if (message.charAt(i)> 64 && message.charAt(i)< 91){
+                int a = ((message.charAt(i)-key-65)%26) +65;
+                end = end + (char)(a);
+            }else{
+                end = end + message.charAt(i);
+            }   
+        }
+        return end;
     }
 
 
